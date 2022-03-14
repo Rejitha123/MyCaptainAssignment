5e06435f7745b522725fc23973ee635ce0a96efc
Rejitha123 <100962677+Rejitha123@users.noreply.github.com>
2022-03-14 23:56:05 +0530

    Add files via upload

---
+++ ar of cir&file ext.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+r=float(input("input the radius of the circle"))
+if(r>0):
+    print("The area of the circle with radius",r,"is",3.14159*r*r)
+
+
+# In[3]:
+
+
+a=input("input the filename:")
+s=a.split(".")
+if(s[1]=="py"):
+    print("The extension of the file is python")
+elif(s[1]=="sql"):
+    print("The extension of the file is structured query language")
+elif(s[1]=="html"):
+    print("The extension of the file is hypertext markup language")
+elif(s[1]=="js"):
+    print("The extension of the file is javascript")
+elif(s[1]=="apk"):
+    print("The extension of the file is android package")
+else:
+    print("Enter another file name!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
