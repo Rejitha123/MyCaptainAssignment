3983629ae7567ac52a27ee545d2b9877fa28fc3b
Rejitha123 <100962677+Rejitha123@users.noreply.github.com>
2022-03-26 21:44:18 +0530

    Add files via upload

---
+++ fibannoci series.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#fibonnaci series
+n=int(input("enter a number to be set as range"))
+a=0
+b=1
+if n==0:
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(2,n):
+        c=a+b
+        a=b
+        b=c
+        print(c)
+
+
+# In[ ]:
+
+
+
+
