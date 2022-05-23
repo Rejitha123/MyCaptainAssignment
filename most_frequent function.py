081c1c99cd625806ed0ae5725c5884356f696010
Rejitha123 <100962677+Rejitha123@users.noreply.github.com>
2022-05-23 21:01:47 +0530

    Add files via upload

---
+++ most_frequent function.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def most_frequent(str1):
+    dict1=dict()
+    for i in str1:
+        if i not in dict1:
+            dict1[i]=1
+        else:
+            dict1[i]+=1
+    print(sorted(dict1.items(),key =
+             lambda kv:(kv[1], kv[0]),reverse=True))
+string=input("enter a string ")
+most_frequent(string)
+
+
+# In[ ]:
+
+
+
+
