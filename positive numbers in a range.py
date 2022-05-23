e941abaa4cbb6d315405accdf752c031446a34c8
Rejitha123 <100962677+Rejitha123@users.noreply.github.com>
2022-05-23 21:16:22 +0530

    Add files via upload

---
+++ positive numbers in a range.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#print positive numbers in a range
+n=eval(input("enter a number to be set as range"))
+i=0
+for i in n:
+    if i>0:
+        print(i)
+
+
+# In[ ]:
+
+
+
+
