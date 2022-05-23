e41ff21c5ff822a77f0122714a02280a688988d7
Rejitha123 <100962677+Rejitha123@users.noreply.github.com>
2022-05-23 21:05:10 +0530

    Update and rename postive n&fibonnaci.py to postive.py

---
+++ postive.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#print positive numbers in a range
+n=int(input("enter a number to be set as range"))
+for i in range(0,n+1):
+    print(i)
+
+
+
+
+
